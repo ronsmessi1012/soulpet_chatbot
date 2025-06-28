e800e2ee43e3b1391bcaa44c85a9ba225759230e
RONNIE <142472363+ronsmessi1012@users.noreply.github.com>
2025-06-29 05:06:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -159,4 +159,4 @@ async def get_suggestions():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
